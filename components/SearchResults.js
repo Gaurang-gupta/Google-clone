e203b45d30125587c687155542af1449a8e17698
Gaurang Gupta <g12172001@gmail.com>
2022-12-07 13:23:13 +0530

    Edited conditions

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -4,19 +4,19 @@ function SearchResults({ results }) {
         <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl[52%]">
             <p className="text-gray-600 text-md mb-5 mt-3">About {results?.searchInformation?.formattedTotalResults} results ({results?.searchInformation?.formattedSearchTime} seconds)</p>
 
-            {results.items?.map((result) => (
+            {results?.items?.map((result) => (
                 <div key={result.link} className="max-w-xl mb-8">
                     <div className="group">
                         <a href={result.link} className="text-sm">
-                            {result.formattedUrl}
+                            {result?.formattedUrl}
                         </a>
-                        <a href={result.link}>
-                            <h2 className="truncate text-xl text-blue-800 font-medium group-hover:underline">{result.title}</h2>
+                        <a href={result?.link}>
+                            <h2 className="truncate text-xl text-blue-800 font-medium group-hover:underline">{result?.title}</h2>
                         </a>
                     </div>
 
                     <p className="line-clamp-2">
-                        {result.snippet}
+                        {result?.snippet}
                     </p>
                 </div>
             ))}
